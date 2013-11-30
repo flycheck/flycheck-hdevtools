1600ef1bd0b78536c198bd9db45a47165534cd86
Sebastian Wiesner <lunaryorn@gmail.com>
2013-11-30 15:51:21 +0100

    Fix development version

--- flycheck-hdevtools.el
+++ flycheck-hdevtools.el
@@ -5,7 +5,7 @@
 ;; Author: Steve Purcell <steve@sanityinc.com>
 ;; Keywords: convenience languages tools
 ;; Package-Requires: ((flycheck "0.15"))
-;; Version: DEV
+;; Version: 0.2-cvs
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
