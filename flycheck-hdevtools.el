68bbf49ca5b25694f827251f9d06dd61336b7e80
Sebastian Wiesner <swiesner@lunaryorn.com>
2014-08-06 18:20:36 +0200

    Make dash dependency explicit

--- flycheck-hdevtools.el
+++ flycheck-hdevtools.el
@@ -5,7 +5,7 @@
 ;; Author: Steve Purcell <steve@sanityinc.com>
 ;; URL: https://github.com/flycheck/flycheck-hdevtools
 ;; Keywords: convenience languages tools
-;; Package-Requires: ((flycheck "0.15"))
+;; Package-Requires: ((flycheck "0.15") (dash "2.0"))
 ;; Version: DEV
 
 ;; This file is not part of GNU Emacs.
@@ -33,6 +33,7 @@
 
 ;;; Code:
 
+(require 'dash)
 (require 'flycheck)
 
 (flycheck-define-checker haskell-hdevtools
